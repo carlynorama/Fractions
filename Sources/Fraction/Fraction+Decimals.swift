4164484286662c2f50607b3fc1662edb3a1d711d
Carlyn Maw <carlynorama@gmail.com>
2021-09-02 12:26:15 -0700

    reorganized, functional farey numbers

--- Sources/Fraction/Fraction+Decimals.swift
+++ Sources/Fraction/Fraction+Decimals.swift
@@ -11,6 +11,27 @@ import Foundation
 //MARK: -- Working with Decimals
 extension Fraction {
     
+    enum DecimalConversionStyle {
+        //Continuous Fraction
+        case fastAndHigh
+        //Farey Number Seeking
+        case closestLowest
+        //Use a value closest to an "Imperial" denominator
+        case snappedToImperial
+    }
+    
+    enum ImperialSnapValue:Double, CaseIterable {
+        case whole = 1.0000000
+        case halves = 0.500000
+        case quarters = 0.250000
+        case eighths = 0.125000
+        case sixteenths = 0.062500
+        case thirtyseconds = 0.031250
+        case sixtyfourths = 0.015625
+        case onetwentyeighths = 0.0078125
+        case twofiftysixths = 0.00390625
+    }
+    
     static func splitDecimal(_ submittedValue:Double, discardTiny:Bool = true) -> (whole:Int, fractional:Double) {
         var whole = Int(submittedValue)
         var fractional = submittedValue - Double(whole)
@@ -55,47 +76,9 @@ extension Fraction {
         return diff < tolerance
     }
 
-    //Slowest
-    static func calcFraction(_ submittedValue:Double, maxDenominator:Int = 100) -> (whole:Int?, numerator:Int, denominator:Int)? {
-        
-        if let whole = Int(exactly: submittedValue) {
-            return (whole: whole, numerator:0, denominator:1)
-        }
-        
-        let split = splitDecimal(submittedValue)
-        
-        for i in 2...maxDenominator {
-            let multipliedFractional = split.fractional * Double(i)
-            
-            if let test = Int(exactly: multipliedFractional) {
-                return (whole: split.whole, numerator: test, denominator:i)
-            }
-            
-        }
-        return nil
-    }
+    //MARK: - Approaches to estimating fractions
     
-    //Never use. Only works if double is gaurenteed to be a rational number.
-    static private func easyRationalsFinder(_ submittedValue:Double, maxDenominator:Int = 100) -> (whole:Int?, numerator:Int, denominator:Int)? {
-        
-        if let whole = Int(exactly: submittedValue) {
-            return (whole: whole, numerator:0, denominator:1)
-        }
-        
-        let split = splitDecimal(submittedValue)
-        
-        for i in 2...maxDenominator {
-            let multipliedFractional = split.fractional * Double(i)
-            
-            if let test = Int(exactly: multipliedFractional) {
-                return (whole: split.whole, numerator: test, denominator:i)
-            }
-            
-        }
-        return nil
-    }
-    
-    //Uses continued fractions to
+    //MARK: -- Continued fractions
     static func speedsterApproximation(of sumittedValue: Double) -> (numerator:Int, denominator:Int) {
         print("Running A algo")
         var x = sumittedValue.magnitude
@@ -118,7 +101,121 @@ extension Fraction {
         return (h * multiplier, k)
     }
     
-    static func snappedToImperial(_ submittedValue:Double, toNearest granularity:ImperialSnapValue = .sixteenths, snapDirection:SnapDirection = .up) -> (whole:Int?, numerator:Int, denominator:Int) {
+    //MARK: -- Farey Numbers
+    //See Numberphiles
+    //Funny Fractions and Ford Circles
+    //https://www.youtube.com/watch?v=0hlvhQZIOQw
+    //Infinite Fractions
+    //https://www.youtube.com/watch?v=DpwUVExX27E
+    //Papers
+    //"Recounting the Rationals" Calkin, Wilf 1999
+    // Also
+    // https://www.cut-the-knot.org/proofs/fords.shtml#mediant
+    
+    
+    //Uses Farey numbers, potentially could end up with a lower denom than the speedster
+    //Mostly clean up to handle numbers not in 0...1
+    static func findFareyApproximation(of submittedDouble:Double, withMaxDenominator maxLimit:Int) -> (Int?, Int, Int) {
+        let neg = submittedDouble < 0
+        let split = splitDecimal(submittedDouble)
+        
+        guard split.fractional.magnitude > 0 else {
+            return (split.whole, 0, 1)
+        }
+        
+        var (numerator, denominator) = fareyAprroximator(of: split.fractional.magnitude, withMaxDenominator: maxLimit)
+
+        
+        let whole = split.whole == 0 ? nil : split.whole
+        
+        if neg && whole == nil {
+            numerator = numerator * -1
+        }
+
+        return (whole, numerator, denominator)
+        
+    }
+
+    //only takes positive numbers between 0 and 1
+    //if it is given a bigger number, the searching algo
+    //has a stride of the whole number (rounded up)
+    static private func fareyAprroximator(of submittedDouble:Double, withMaxDenominator maxLimit:Int) -> (Int, Int){
+        var pair1 = (0,1)
+        var pair2 = (1, 1)
+        
+        while pair1.1 <= maxLimit && pair2.1 <= maxLimit {
+            let mendiantPair = fareyAddition(f1: pair1, f2: pair2)
+            let mendiantDouble = Double(mendiantPair.0)/Double(mendiantPair.1)
+            
+            //If we lucked into it return, else snuggle the pairs in
+            switch submittedDouble {
+            case mendiantDouble:
+                return mendiantPair
+            case let x where x < mendiantDouble:
+                pair2 = mendiantPair
+            default:
+                pair1 = mendiantPair
+            }
+            
+            //print("pair1:\(pair1), pair2:\(pair2)")
+            
+        }
+        
+        //print("pair1:\(pair1), pair2:\(pair2)")
+        let lowerDenomPair = pair1.1 > maxLimit ? pair2 : pair1
+        return lowerDenomPair
+    }
+
+    //Also mendiants and fresman sums
+    private static func fareyAddition(f1:(Int, Int), f2:(Int, Int)) -> (Int, Int){
+        let n = f1.0 + f2.0
+        let d = f1.1 + f2.1
+        return (n, d)
+    }
+
+    private static func fareyShift(pair:(Int, Int)) -> (Int, Int) {
+        let x = pair.0 + pair.1
+        let y = pair.1
+        return (x,y)
+    }
+
+    //stern-brocot sequence?
+    private static func fareySequence(iterations:Int) {
+        var pair = (0,1)
+        var sequence:[Int] = []
+        for i in 0...iterations {
+            let newPair = fareyShift(pair: pair)
+            sequence.append(newPair.0)
+            sequence.append(newPair.1)
+            pair = (sequence[i], sequence[i+1])
+        }
+        print(sequence)
+    }
+
+    //Not very useful...
+    private static func fareySequence(maxValue:Int) {
+        //let iterations = 100000
+        var i = 0
+        var pair = (0,1)
+        var sequence:[Int] = []
+        while !sequence.contains(maxValue+1) {
+            let newPair = fareyShift(pair: pair)
+            if newPair.1 == (maxValue+1) || newPair.0 == maxValue+1 {
+                break
+            }
+            sequence.append(newPair.0)
+            sequence.append(newPair.1)
+            pair = (sequence[i], sequence[i+1])
+            //print(pair)
+            i = i + 1
+        }
+        print(sequence)
+    }
+    
+    //MARK: -- Snaping to Given Denominators
+
+    //Should find the lowest... kinda very slow
+    static func snappedToImperial(_ submittedValue:Double, toNearest granularity:ImperialSnapValue = .sixtyfourths, snapDirection:SnapDirection = .up) -> (whole:Int?, numerator:Int, denominator:Int) {
         
         var currentlysnapsto:ImperialSnapValue?
         //does it already snap?
@@ -159,22 +256,13 @@ extension Fraction {
         case down
     }
     
-    enum ImperialSnapValue:Double, CaseIterable {
-        case whole = 1.0000000
-        case halves = 0.500000
-        case quarters = 0.250000
-        case eighths = 0.125000
-        case sixteenths = 0.062500
-        case thirtyseconds = 0.031250
-        case sixtyfourths = 0.015625
-    }
+
+
     
-    enum DecimalConversionStyle {
-        //Continuous Fraction
-        case fastAndHigh
-        //Farey Number Seeking
-        case closestLowest
-        //Use a value closest to an "Imperial" denominator
-        case snappedToImperial
-    }
+    //MARK: - Farey Numbers
+    
+
+ 
+    
+//END EXTENTION
 }
